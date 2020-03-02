b15098ce40f3930e2aee7eb367ad2144a3e3bd34
Gadi Naor <gadi@alcide.io>
2020-03-02 09:08:55 +0200

    fix test flow

--- advisor-scan.sh
+++ advisor-scan.sh
@@ -18,116 +18,7 @@ set -o errexit
 set -o nounset
 set -o pipefail
 
-DEFAULT_KIND_VERSION=v0.7.0
-DEFAULT_CLUSTER_NAME=chart-testing
-KUBECTL_VERSION=v1.17.0
-
-show_help() {
-cat << EOF
-Usage: $(basename "$0") <options>
-
-    -h, --help                              Display help
-    -v, --version                           The kind version to use (default: v0.7.0)"
-    -c, --config                            The path to the kind config file"
-    -i, --node-image                        The Docker image for the cluster nodes"
-    -n, --cluster-name                      The name of the cluster to create (default: chart-testing)"
-    -w, --wait                              The duration to wait for the control plane to become ready (default: 60s)"
-    -l, --log-level                         The log level for kind [panic, fatal, error, warning, info, debug, trace] (default: warning)
-
-EOF
-}
-
-main() {
-    local version="$DEFAULT_KIND_VERSION"
-    local config=
-    local node_image=
-    local cluster_name="$DEFAULT_CLUSTER_NAME"
-    local wait=60s
-    local log_level=
-
-    parse_command_line "$@"
-
-    install_kind
-    install_kubectl
-    create_kind_cluster
-
-}
-
-parse_command_line() {
-    while :; do
-        case "${1:-}" in
-            -h|--help)
-                show_help
-                exit
-                ;;
-            -v|--version)
-                if [[ -n "${2:-}" ]]; then
-                    version="$2"
-                    shift
-                else
-                    echo "ERROR: '-v|--version' cannot be empty." >&2
-                    show_help
-                    exit 1
-                fi
-                ;;
-            -c|--config)
-                if [[ -n "${2:-}" ]]; then
-                    config="$2"
-                    shift
-                else
-                    echo "ERROR: '--config' cannot be empty." >&2
-                    show_help
-                    exit 1
-                fi
-                ;;
-            -i|--node-image)
-                if [[ -n "${2:-}" ]]; then
-                    node_image="$2"
-                    shift
-                else
-                    echo "ERROR: '-i|--node-image' cannot be empty." >&2
-                    show_help
-                    exit 1
-                fi
-                ;;
-            -n|--cluster-name)
-                if [[ -n "${2:-}" ]]; then
-                    cluster_name="$2"
-                    shift
-                else
-                    echo "ERROR: '-n|--cluster-name' cannot be empty." >&2
-                    show_help
-                    exit 1
-                fi
-                ;;
-            -w|--wait)
-                if [[ -n "${2:-}" ]]; then
-                    wait="$2"
-                    shift
-                else
-                    echo "ERROR: '--wait' cannot be empty." >&2
-                    show_help
-                    exit 1
-                fi
-                ;;
-            -l|--log-level)
-                if [[ -n "${2:-}" ]]; then
-                    log_level="$2"
-                    shift
-                else
-                    echo "ERROR: '--log-level' cannot be empty." >&2
-                    show_help
-                    exit 1
-                fi
-                ;;
-            *)
-                break
-                ;;
-        esac
-
-        shift
-    done
-}
+SCRIPT_DIR=$(dirname -- "$(readlink -f "${BASH_SOURCE[0]}" || realpath "${BASH_SOURCE[0]}")")
 
 alcide_download_advisor(){
     echo "Downloading Alcide Advisor"
@@ -146,12 +37,41 @@ alcide_download_advisor(){
     chmod +x kube-advisor
 }
 
-alcide_scan_cluster(){
-    local outdir=$1
-    local context=$2
-    
-    ./kube-advisor --eula-sign validate cluster --namespace-include="*" --outfile $outdir/$context.html
+main() {
+    args=()
+
+    if [[ -n "${INPUT_FAIL_ON_CRITICAL:-}" ]]; then
+        args+=(--run-mode=pipeline)
+    fi
+
+    if [[ -n "${INPUT_OUTPUT_FILE:-}" ]]; then
+        args+=(--outfile "${INPUT_OUTPUT_FILE}")
+    fi
+
+    if [[ -n "${INPUT_INCLUDE_NAMESPACES:-}" ]]; then
+        args+=(--namespace-include "${INPUT_INCLUDE_NAMESPACES}")
+    fi   
+
+    if [[ -n "${INPUT_EXCLUDE_NAMESPACES:-}" ]]; then
+        args+=(--namespace-exclude "${INPUT_EXCLUDE_NAMESPACES}")
+    fi   
+
+    if [[ -n "${INPUT_POLICY_PROFILE:-}" ]]; then
+        args+=(--policy-profile "${INPUT_POLICY_PROFILE}")
+    fi
+
+    if [[ -n "${INPUT_POLICY_PROFILE_ID:-}" ]]; then
+        args+=(--profile-id "${INPUT_POLICY_PROFILE_ID}")
+        args+=(--alcide-api-key "${INPUT_ALCIDE_APIKEY}")
+        args+=(--alcide-api-server "${INPUT_ALCIDE_APISERVER}")        
+    fi
+
+    # Download fresh scanner
+    alcide_download_advisor 
+
+    # Scan the cluster
+    ./kube-advisor --eula-sign validate cluster  "${args[@]}"
 }
 
+main
 
-main "$@"
